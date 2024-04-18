c2631871ebb4ac416f31d8be2ca8ee0addd7b2eb
Giovanni Zanin <giovannizanin00@gmail.com>
2024-04-18 16:41:42 +0200

    commit

--- TennisMateApp/Consigliati.js
+++ TennisMateApp/Consigliati.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {Text} from 'react-native';
 
 function Consigliati() {
-return <Text>Profili consigliati</Text>;
+return <Text>Profili consigliatiiiiiiiiii</Text>;
 }
 
 export default Consigliati
